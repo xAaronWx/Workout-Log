4c707dda4e65b09a6d0eb59d82d8e27efdb08fad
xAaronWx <50504602+xAaronWx@users.noreply.github.com>
2021-01-23 22:16:42 -0500

    login endpoint

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -21,4 +21,33 @@ router.post("/register", function (req, res) {
     .catch((err) => res.status(500).json({ error: err }));
 });
 
+router.post("/login", function (req, res) {
+  User.findOne({ where: { username: req.body.username } })
+    .then(function loginSuccess(user) {
+      if (user) {
+        bcrypt.compare(
+          req.body.passwordhash,
+          user.passwordhash,
+          function (err, matches) {
+            if (matches) {
+              let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+                expiresIn: 60 * 60 * 24,
+              });
+              res.status(200).json({
+                user: user,
+                message: "Login Success",
+                sessionToken: token,
+              });
+            } else {
+              res.status(502).send({ error: "Login failed" });
+            }
+          }
+        );
+      } else {
+        res.status(500).json({ error: "User does not exist." });
+      }
+    })
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
 module.exports = router;
