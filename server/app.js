833fc8107fefa32c73d24b8b5a05a18188453acc
xAaronWx <50504602+xAaronWx@users.noreply.github.com>
2021-01-23 22:38:53 -0500

    Added Headers

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ let user = require("./controllers/usercontroller");
 const router = require("./controllers/usercontroller");
 
 sequelize.sync();
-// app.use(require("./middleware/headers"));
+app.use(require("./middleware/headers"));
 app.use(express.json());
 
 app.use("/user", user);
--- server/controllers/logcontroller.js
+++ server/controllers/logcontroller.js
@@ -3,4 +3,19 @@ let router = express.Router();
 let validateSession = require("../middleware/validate-session");
 const Journal = require("../db").import("../models/journal");
 
+router.post("/log", validateSession, (req, res) => {
+  const logEntry = {
+    description: req.body.description,
+    definition: req.body.definition,
+    result: req.body.result,
+    owner_id: req.user.id,
+  };
+
+  Log.create(logEntry)
+    .then((log) => res.status(200).json(log))
+    .catch((err) =>
+      res.status(500).json({ error: err, message: "Log not created" })
+    );
+});
+
 module.exports = router;
